64be59142770034880eb74c189c7f10288dc4068
ramirotomic <ramiro.tomic00@gmail.com>
2018-08-03 11:18:04 -0300

    hice cosasssfyrdesydr

--- Main BDD.py
+++ Main BDD.py
@@ -3,8 +3,38 @@ from Artista import Artista
 from Cancion import Cancion
 from Genero import Genero
 
-p = Artista('Purpura','Cabildo 2506 - 25/09')
+p = Artista()
 
-z = p.LeerArt()
-for a in z:
-    print(a.Nombre, " - ",a.Conciertos)
+menu = {}
+menu['1']="Insertar Artista"
+menu['2']="Leer Artista"
+menu['3']="Eliminar Artista"
+menu['4']="Modificar Artista"
+menu['5']="Insertar Album"
+menu['6']="Leer Album"
+menu['7']="Eliminar Album"
+menu['8']="Modificar Album"
+menu['9']="Insertar Cancion"
+menu['10']="Leer Cancion"
+menu['11']="Eliminar Cancion"
+menu['12']="Modificar Cancion"
+menu['13']="Insertar Genero"
+menu['14']="Leer Genero"
+menu['15']="Eliminar Genero"
+menu['16']="Modificar Genero"
+menu['17']="Exit"
+while True:
+
+    selection=input("Please Select:")
+    if selection =='1':
+        p.InsertArt('Purpura','Condarco 2506')
+    elif selection == '2':
+        z = p.LeerArt()
+        for a in z:
+            print(a.Nombre, " - ", a.Conciertos)
+    elif selection == '3':
+
+    elif selection == '17':
+      break
+    else:
+      print ("Unknown Option Selected!")
